8ae9bb271505545e1ff24119e16c0a8ee5ee7284
Joey Whalen <jtktwhalen@gmail.com>
2021-08-05 19:41:00 -0400

    Created ArticleRepo.

--- src/main/java/org/wcci/transportationev/project/storage/ArticleTopicRepository.java
+++ src/main/java/org/wcci/transportationev/project/storage/ArticleTopicRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.wcci.transportationev.project.resources.ArticleTopic;
 
 public interface ArticleTopicRepository extends CrudRepository<ArticleTopic, Long> {
-    
+
 }
