451199303d954f133f5c343322ce66538c81469c
George Fought <84338581+GeorgeFought@users.noreply.github.com>
2021-08-06 14:42:16 -0400

    Revert "Jawe vehicle"

--- src/main/java/org/wcci/transportationev/project/storage/ElectricVehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package org.wcci.transportationev.project.storage;
-
-import org.springframework.data.repository.CrudRepository;
-import org.wcci.transportationev.project.resources.ElectricVehicle;
-
-public interface ElectricVehicleRepository  extends CrudRepository<ElectricVehicle, Long> {
-
-}
