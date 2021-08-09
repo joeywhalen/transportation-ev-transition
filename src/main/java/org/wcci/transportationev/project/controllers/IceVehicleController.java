fd85e54c486b47ff0638cb6a478a5a7c0c2ce937
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-08-09 16:12:54 -0400

    Added controller for specific models of a make

--- src/main/java/org/wcci/transportationev/project/controllers/IceVehicleController.java
+++ src/main/java/org/wcci/transportationev/project/controllers/IceVehicleController.java
@@ -1,5 +1,7 @@
 package org.wcci.transportationev.project.controllers;
 
+import java.util.Collection;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,27 +20,46 @@ public class IceVehicleController {
     private ModelStorage modelStorage;
     private IceVehicleStorage iceVehicleStorage;
 
-    public IceVehicleController(YearStorage yearStorage, MakeStorage makeStorage,
-    ModelStorage modelStorage,IceVehicleStorage iceVehicleStorage){
+    public IceVehicleController(YearStorage yearStorage, MakeStorage makeStorage, ModelStorage modelStorage,
+            IceVehicleStorage iceVehicleStorage) {
         this.yearStorage = yearStorage;
         this.makeStorage = makeStorage;
         this.modelStorage = modelStorage;
         this.iceVehicleStorage = iceVehicleStorage;
     }
 
-    //GET http://localhost:8080/api/ice/years
+    // GET http://localhost:8080/api/ice/years
 
     @GetMapping("/api/ice/years")
-    public Iterable<Year> retrieveAllYears(){
+    public Iterable<Year> retrieveAllYears() {
         return yearStorage.retrieveAllYears();
     }
 
-//     ### Get all makes of said year
-// GET http://localhost:8080/api/ice/years/2012
+    // ### Get all makes of said year
+    // GET http://localhost:8080/api/ice/years/2012
     @GetMapping("/api/ice/years/{year}")
-    public Iterable<Make> retrieveAllMakesByYear(@PathVariable int year){
+    public Iterable<Make> retrieveAllMakesByYear(@PathVariable int year) {
         Year currentYear = yearStorage.retrieveMakesByYear(year);
         return currentYear.getMakes();
     }
-}
 
+    // ### Get all models of said make
+    // GET http://localhost:8080/api/ice/years/2012/ford
+
+    @GetMapping("/api/ice/years/{year}/{make}")
+    public Make retrieveMakeByYear(@PathVariable int year, @PathVariable String make) {
+
+        Year currentYear = yearStorage.retrieveMakesByYear(year);
+        Collection<Make> currentMakes = currentYear.getMakes();
+
+        Make makeToReturn = null;
+
+        for (Make currentMake : currentMakes) {
+            if (currentMake.getMakeName().toLowerCase().equals(make)) {
+                makeToReturn = currentMake;
+            }
+        }
+        return makeToReturn;
+    }
+
+}
