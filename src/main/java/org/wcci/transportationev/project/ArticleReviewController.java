a0664b7ac95467f888549f653cf2e2283ceac91a
Joey Whalen <jtktwhalen@gmail.com>
2021-08-09 14:24:01 -0400

    Building the AlbumReviewController based on HTTP requests.

--- src/main/java/org/wcci/transportationev/project/ArticleReviewController.java
+++ src/main/java/org/wcci/transportationev/project/ArticleReviewController.java
@@ -8,6 +8,18 @@ public class ArticleReviewController {
     private ReviewTopicStorage reviewTopicStorage;
     private ReviewStorage reviewStorage;
 
-    
+    public ArticleReviewController(ArticleTopicStorage articleTopicStorage, ArticleStorage articleStorage, 
+                        ReviewTopicStorage reviewTopicStorage, ReviewStorage reviewStorage) {
+        this.articleTopicStorage = articleTopicStorage;
+        this.articleStorage = articleStorage;
+        this.reviewTopicStorage = reviewTopicStorage;
+        this.reviewStorage = reviewStorage;
+    }
+
+    //GET http://localhost:8080/api/articleTopics
+    @GetMapping("/api/articleTopics")
+    public Iterable<ArticleTopic> retrieveAllArticleTopics() {
+        return articleTopicStorage.retrieveAllArticleTopics();
+    }
     
 }
