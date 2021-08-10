2a708948a13c21fd20d47d81ce05cf2b91726ca9
George Fought <84338581+GeorgeFought@users.noreply.github.com>
2021-08-10 14:30:03 -0400

    Added forgotten types of GasPrice, Long to GasPriceRepository.

--- src/main/java/org/wcci/transportationev/project/storage/GasPriceRepository.java
+++ src/main/java/org/wcci/transportationev/project/storage/GasPriceRepository.java
@@ -1,6 +1,7 @@
 package org.wcci.transportationev.project.storage;
 
 import org.springframework.data.repository.CrudRepository;
+import org.wcci.transportationev.project.resources.GasPrice;
 
-public interface GasPriceRepository extends CrudRepository {
+public interface GasPriceRepository extends CrudRepository<GasPrice, Long> {
 }
