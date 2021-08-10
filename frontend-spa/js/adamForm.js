ed5adf7912b99333d295b1d42ce1242a743077de
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-08-10 14:58:34 -0400

    Deleted old js files for form

--- frontend-spa/js/adamForm.js
+++
@@ -1,9 +0,0 @@
-// import {genYears,clearChildren} from "./userVehicleForm.js"
-
-// const container = document.querySelector(".container")
-
-// fetch("http://localhost:8080/api/ice/years")
-//     .then(response => response.json())
-//     .then(years => genYears(years))
-//     .catch(error => console.log(error))
-
