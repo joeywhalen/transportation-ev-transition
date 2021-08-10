4e01096349387668ddcbf1e38282026ba507e33c
Joey Whalen <jtktwhalen@gmail.com>
2021-08-10 16:29:28 -0400

    Creating ChargeCostPopulator.  Added two states.

--- src/main/java/org/wcci/transportationev/project/ChargeCostPopulator.java
+++ src/main/java/org/wcci/transportationev/project/ChargeCostPopulator.java
@@ -2,6 +2,7 @@ package org.wcci.transportationev.project;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.wcci.transportationev.project.resources.ChargeCost;
 import org.wcci.transportationev.project.storage.ChargeCostRepository;
 
 @Component
@@ -16,6 +17,12 @@ public class ChargeCostPopulator implements CommandLineRunner {
     
     @Override
     public void run(String... args) throws Exception {
+        ChargeCost alabama = new ChargeCost("Alabama", 7.94, 3333, 11.68, 2272);
+        chargeCostRepository.save(alabama);
+
+        ChargeCost alaska = new ChargeCost("Alaska", 13.59, 1923, 16.32, 1613);
+        chargeCostRepository.save(alaska);
+
         
     }
     
