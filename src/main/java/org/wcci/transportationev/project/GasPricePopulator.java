da2d06504ea9bc46a374d6e8c56f0604a2b74dae
Joey Whalen <jtktwhalen@gmail.com>
2021-08-10 21:36:29 -0400

    Adding data to GasPricePopulator.

--- src/main/java/org/wcci/transportationev/project/GasPricePopulator.java
+++ src/main/java/org/wcci/transportationev/project/GasPricePopulator.java
@@ -17,52 +17,76 @@ public class GasPricePopulator implements CommandLineRunner {
     public void run(String... args) throws Exception {
         GasPrice alabama = new GasPrice("Alabama", 2.84f, 1060);
         gasPriceRepository.save(alabama);
+
         GasPrice alaska = new GasPrice("Alaska", 3.67f, 824);
         gasPriceRepository.save(alaska);
+
         GasPrice arizona = new GasPrice("Arizona", 3.12f, 961);
         gasPriceRepository.save(arizona);
+
         GasPrice arkansas = new GasPrice("Arkansas", 2.87f, 1052);
         gasPriceRepository.save(arkansas);
+
         GasPrice california = new GasPrice("California", 4.39f, 693);
         gasPriceRepository.save(california);
+
         GasPrice colorado = new GasPrice("Colorado", 3.63f, 847);
         gasPriceRepository.save(colorado);
+
         GasPrice connecticut = new GasPrice("Connecticut", 3.17f, 952);
         gasPriceRepository.save(connecticut);
+
         GasPrice dc = new GasPrice("District of Columbia", 3.29f, 923);
         gasPriceRepository.save(dc);
+
         GasPrice delaware = new GasPrice("Delaware", 3.01f, 984);
         gasPriceRepository.save(delaware);
+
         GasPrice florida = new GasPrice("Florida", 3.01f, 1009);
         gasPriceRepository.save(florida);
+
         GasPrice georgia = new GasPrice("Georgia", 2.96f, 1011);
         gasPriceRepository.save(georgia);
+
         GasPrice hawaii = new GasPrice("Hawaii", 4.09f, 736);
         gasPriceRepository.save(hawaii);
+
         GasPrice idaho = new GasPrice("Idaho", 3.80f, 800);
         gasPriceRepository.save(idaho);
+
         GasPrice illinois = new GasPrice("Illinois", 3.40f, 890);
         gasPriceRepository.save(illinois);
+
         GasPrice indiana = new GasPrice("Indiana", 3.12f, 963);
         gasPriceRepository.save(indiana);
+
         GasPrice iowa = new GasPrice("Iowa", 2.99f, 1005);
         gasPriceRepository.save(iowa);
+
         GasPrice kansas = new GasPrice("Kansas", 2.93f, 1028);
         gasPriceRepository.save(kansas);
-        GasPrice kentucky = new GasPrice("Kentucky",2.96f);
+
+        GasPrice kentucky = new GasPrice("Kentucky", 2.96f, 1031);
         gasPriceRepository.save(kentucky);
-        GasPrice louisiana = new GasPrice("Louisiana",2.82f);
+
+        GasPrice louisiana = new GasPrice("Louisiana", 2.82f, 1067);
         gasPriceRepository.save(louisiana);
-        GasPrice mass = new GasPrice("Massachusetts",3.04f);
-        gasPriceRepository.save(mass);
-        GasPrice mary = new GasPrice("Maryland",3.06f);
-        gasPriceRepository.save(mary);
-        GasPrice maine = new GasPrice("Maine",3.11f);
+
+        GasPrice maine = new GasPrice("Maine", 3.11f, 964);
         gasPriceRepository.save(maine);
-        GasPrice mich = new GasPrice("Michigan",3.28f);
+
+        GasPrice mary = new GasPrice("Maryland", 3.06f, 976);
+        gasPriceRepository.save(mary);
+
+        GasPrice mass = new GasPrice("Massachusetts", 3.04f, 992);
+        gasPriceRepository.save(mass);
+        
+        GasPrice mich = new GasPrice("Michigan", 3.28f, 933);
         gasPriceRepository.save(mich);
-        GasPrice minn = new GasPrice("Minnesota",3.03f);
+
+        GasPrice minn = new GasPrice("Minnesota", 3.03f, 1001);
         gasPriceRepository.save(minn);
+        
         GasPrice missouri = new GasPrice("Missouri",2.87f);
         gasPriceRepository.save(missouri);
         GasPrice mississippi = new GasPrice("Mississippi",2.79f);
