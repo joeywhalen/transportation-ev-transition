8a47b1ceba237404e82cb612b9ee0b571ca4f79b
Joey Whalen <jtktwhalen@gmail.com>
2021-08-12 19:34:52 -0400

    Building displayArticle.js to display a single article.

--- frontend-spa/js/displayArticle.js
+++ frontend-spa/js/displayArticle.js
@@ -4,13 +4,19 @@ const displayArticle = function (article) {
     const mainElement = document.querySelector(".main-content");
     const articleElement = document.createElement("div");
     articleElement.classList.add("article-content");
-    const articleTitleElement = document.createElement("h1");
+    const articleTopicElement = document.createElement("h1");
+    articleTopicElement.innerText = article.articleTopic;
+    const articleTitleElement = document.createElement("h2");
     articleTitleElement.innerText = article.articleTitle;
     const articleImageElement = document.createElement("img");
     articleImageElement.src = article.imageUrl;
     const articleAuthorElement = document.createElement("h3");
     articleAuthorElement.innerText = article.authorName;
-    
+    const articleTextElement = document.createElement("p");
+    articleTextElement.innerText = article.articleContent;
+    const articleCommentsNotationElement = document.createElement("article-comments");
+    articleCommentsNotationElement.classList.add("article-comments-notation");
+    articleCommentsNotationElement.innerText = "Comments: ";
 }
 
 
@@ -18,7 +24,8 @@ const displayArticle = function (article) {
 
 
 {/* <body>
-    <h1 class="single-article-title"> Electric Vehicle Benefits and Considerations</h1>
+    <h1 class="single-article-topic">Why We Should Transition To EVs</h1>
+    <h2 class="single-article-title"> Electric Vehicle Benefits and Considerations</h2>
     <img class="single-article-img"
         src="https://www.zerohedge.com/s3/files/inline-images/2019-07-21_13-21-38.png?itok=NQxqZjHQ">
     <h3 class="single-article-author"> By: Jackie Brown<h3>
