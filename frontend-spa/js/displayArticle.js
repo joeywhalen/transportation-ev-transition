520d85af98dec769775f41d7d0200eb258ddcc02
Joey Whalen <jtktwhalen@gmail.com>
2021-08-12 19:05:24 -0400

    Building displayArticle.js to display a single article.

---
+++ frontend-spa/js/displayArticle.js
@@ -0,0 +1,9 @@
+
+
+const displayArticle = function (article) {
+    const mainElement = document.querySelector(".main-content");
+    const articleElement = document.createElement("div");
+    articleElement.classList.add("article-content");
+    const articleTitleElement = document.createElement("h1");
+    
+}
