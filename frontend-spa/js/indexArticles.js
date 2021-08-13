d5ffe7b702a01cf66b0c70a17388785068ef4035
Joey Whalen <jtktwhalen@gmail.com>
2021-08-13 10:58:01 -0400

    Added functionality and http request to retrieve all articles.

---
+++ frontend-spa/js/indexArticles.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+{/* <div class="article-section">
+        
+        <div class="rectangle">
+            <p class="articles-title">Articles</p>
+        </div> 
+        <div class="articles-wrapper">
+            <div class="articles-grid">
+              <div class="article1">
+                  <a href="/frontend-spa/single-article-prototype.html">
+                    <h3>This is an article</h3>
+                    <p>You can find all kinds of car related things here.</p>
+                 </a>
+              </div>
+              <div class="article2">
+                  <a href="/frontend-spa/single-article-prototype.html">
+                     <h3>This is also an article</h3>
+                     <p>blah blah blah blah</p>
+                 </a>
+              </div>
+              <div class="article3">
+                  <a href="/frontend-spa/single-article-prototype.html">
+                    <h3>Cars are really cool</h3>
+                    <p>blahhhhhhhhhhh</p>
+                 </a>
+              </div>
+              <div class="article4">
+                  <a href="/frontend-spa/single-article-prototype.html">
+                    <h3>i love cars</h3>
+                    <p>i really..... love cars </p>
+                </a>
+              </div>
+              <div class="article5">
+                  <a href="/frontend-spa/single-article-prototype.html">
+                    <h3>This is another article</h3>
+                    <p>car stuff</p>
+                 </a>
+            </div>
+              <div class="article6">
+                  <a href="/frontend-spa/single-article-prototype.html">
+                    <p>cars cars cars cars carrrrs </p>
+                 </a>
+                </div>
+            </div>
+            </div>
+        </div> */}
