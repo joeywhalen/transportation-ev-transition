2fa0c004ce230e9b56b68b1bb0ebaa46d4cdcdbb
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-08-13 16:26:53 -0400

    Fixed fetching issue and grid styling

--- frontend-spa/js/app.js
+++ frontend-spa/js/app.js
@@ -3,21 +3,23 @@ import {
 } from "./Header.js"
 
 import {
-    displayHomeBody
-} from "./body.js"
+     displayAllVehicles
+ } from "./display-all-vehicle.js"
 
-// import {
-//     displayFooter
-// } from "./footer.js"
+ import {
+     displayFooter
+ } from "./footer.js"
+
+const body = document.querySelector(".body")
+
+body.append(displayHeader());
+
+await fetch("http://localhost:8080/api/electricVehicles")
+.then(response => response.json())
+.then(allElectricVehicles => displayAllVehicles(allElectricVehicles))
+.catch(error => console.log(error))
+
+body.append(displayFooter());
 
 
-
-
-
-const wrapper = document.querySelector(".wrapper")
-wrapper.append(displayHeader());
-
-wrapper.append(displayHomeBody());
-
-// wrapper.append(displayFooter());
 
--- frontend-spa/js/display-all-vehicle.js
+++ frontend-spa/js/display-all-vehicle.js
@@ -1,69 +1,66 @@
+const displayAllVehicles = function (ElectricVehicles) {
 
-const displayAllVehicles = function(ElectricVehicles) {
-    // const body = document.body;
-    // const wrapper = document.createElement("div");
-    // wrapper.classList.add("wrapper");
-    // body.append(wrapper);
 
-    // const pageTitle = document.createElement("div");
-    // pageTitle.classList.add("vehicle-page-title");
-    // pageTitle.innerHTML = '<h1 class="title-text">Check Out Our Wide Selection of EVs</h1>'
-    // body.append(pageTitle);
+    const body = document.querySelector(".body")
 
-    // const gridContainer = document.createElement("div");
-    // gridContainer.classList.add("grid-container");
-    // body.append(gridContainer);
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+    body.append(wrapper);
+
+    const pageTitle = document.createElement("div");
+    pageTitle.classList.add("vehicle-page-title");
+    pageTitle.innerHTML = '<h1 class="title-text">Check Out Our Wide Selection of EVs</h1>'
+    body.append(pageTitle);
+
+
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+    body.append(gridContainer);
 
     ElectricVehicles.forEach((ElectricVehicle) => {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    // body.append(gridItem);
 
-    const vehicle1 = document.createElement("img");
-    vehicle1.setAttribute("src", "images/2015-Chevrolet-BoltEV-Concept-exterior-001.jpg");
-    vehicle1.setAttribute("id", "vehicle1");
-    // gridItem.append(vehicle1);
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridContainer.appendChild(gridItem);
 
-    const textContainer = document.createElement("div");
-    textContainer.classList.add("text-container");
-    // gridItem.append(textContainer);
+        const vehicle1 = document.createElement("img");
+        vehicle1.setAttribute("src", "images/2015-Chevrolet-BoltEV-Concept-exterior-001.jpg");
+        vehicle1.setAttribute("id", "vehicle1");
+        gridItem.appendChild(vehicle1);
 
-    const vehicleName = document.createElement("p");
-    vehicleName.innerText =ElectricVehicle.;
-    // textContainer.append(vehicleName);
+        const textContainer = document.createElement("div");
+        textContainer.classList.add("text-container");
+        gridItem.appendChild(textContainer);
 
-    const vehiclePrice = document.createElement("p");
-    vehiclePrice.innerText = "$18,000";
-    // textContainer.append(vehiclePrice);
+        const vehicleName = document.createElement("p");
+        vehicleName.innerText = "Vehicle Name";
+        textContainer.appendChild(vehicleName);
 
-    const mPG = document.createElement("p");
-    mPG.innerText = "60mpg";
-    // textContainer.append(mPG);
+        const vehiclePrice = document.createElement("p");
+        vehiclePrice.innerText = "$18,000";
+        textContainer.appendChild(vehiclePrice);
 
-    const exploreButton = document.createElement("button");
-    exploreButton.classList.add("explore-button");
-    exploreButton.innerHTML = '<button class="explore-button"><a></a>Explore</button>'
-    // textContainer.append(exploreButton);
+        const mPG = document.createElement("p");
+        mPG.innerText = "60mpg";
+        textContainer.appendChild(mPG);
+
+        const exploreButton = document.createElement("button");
+        exploreButton.classList.add("explore-button");
+        exploreButton.innerText = "Explore"
+        textContainer.appendChild(exploreButton);
 
 
-    gridItem.addEventListener("click", () => {
-        displayAllVehicles(ElectricVehicle, ElectricVehicles)
-    })
-    body.append(gridItem);
-    gridItem.append(vehicle1);
-    gridItem.append(textContainer);
-    textContainer.append(vehicleName);
-    textContainer.append(vehiclePrice);
-    textContainer.append(mPG);
-    textContainer.append(exploreButton);
+        // gridItem.addEventListener("click", () => {
+
+
+        //     displaySingleEV(ElectricVehicle)
+        // })
+
 
     })
 
+}
 
-    
-
-
-
-
-
+export {
+    displayAllVehicles
 }
