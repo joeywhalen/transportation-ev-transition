753edc809b5d6050dccc7dde75a62bbdb9a7d591
micaela <micky.diloreto@gmail.com>
2021-08-17 17:05:24 -0400

    messed around with modal

--- frontend-spa/js/Header.js
+++ frontend-spa/js/Header.js
@@ -1,12 +1,12 @@
-// import {
-//   modal
-// } from "./modal.js"
+import {
+  contactsModal
+} from "./modal.js"
 
 // Displays header 
 const displayHeader = function () {
     const header = document.createElement("header");
     header.classList.add("header-container");
-    header.innerHTML = '<div class="header"><ul><li class="website-title"><h1>Compar<span>EV</span></h1></li></ul></div><input type="checkbox" class="openSidebarMenu" id="openSidebarMenu"><label for="openSidebarMenu" class="sidebarIconToggle"><div class="spinner diagonal part-1"></div><div class="spinner horizontal"></div><div class="spinner diagonal part-2"></div></label><div id="sidebarMenu"><ul class="sidebarMenuInner"><li><a href="./index.html">Home</a></li><li><a href="./all-evs.html" target="_blank">Vehicles</a></li><li><a href="">Articles</a></li><li><a href="">Contacts</a></li></ul></div>'
+    header.innerHTML = '<div class="header"><ul><li class="website-title"><h1>Compar<span>EV</span></h1></li></ul></div><input type="checkbox" class="openSidebarMenu" id="openSidebarMenu"><label for="openSidebarMenu" class="sidebarIconToggle"><div class="spinner diagonal part-1"></div><div class="spinner horizontal"></div><div class="spinner diagonal part-2"></div></label><div id="sidebarMenu"><ul class="sidebarMenuInner"><li><a href="./index.html">Home</a></li><li><a href="./all-evs.html">Vehicles</a></li><li><a href="">Articles</a></li><li><a href="./all-article-topics.html">Contacts</a></li></ul></div>'
 
     return header;
 
--- frontend-spa/js/modal.js
+++ frontend-spa/js/modal.js
@@ -1,24 +1,39 @@
 
-      //    // modal
-      
-      // var modal = document.getElementById("myModal");
-      // var btn = document.getElementById("myBtn");
-      // var span = document.getElementsByClassName("close")[0];
+      const contactsModal = function () {
+
+            var modal = document.getElementById("myModal");
+            var btn = document.getElementById("myBtn");
+            var span = document.getElementsByClassName("close")[0];
+                  
+            btn.onclick = function() {
+              modal.style.display = "block";
+            }
             
-      // btn.onclick = function() {
-      //   modal.style.display = "block";
-      // }
+            span.onclick = function() {
+              modal.style.display = "none";
+            }
+            
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            }
+      }
       
-      // span.onclick = function() {
-      //   modal.style.display = "none";
-      // }
-      
-      // window.onclick = function(event) {
-      //   if (event.target == modal) {
-      //     modal.style.display = "none";
-      //   }
-      // }
-
-      // export {modal}
-
      
+
+      export {contactsModal}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
